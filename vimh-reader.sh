93602aec0cde5b2c79648e6f7b87cd7e00ddcc5f
Matthew L Davis <mld.0@protonmail.com>
2022-06-04 03:01:34 +1000

    ongoing

--- vimh-reader.sh
+++ vimh-reader.sh
@@ -17,8 +17,18 @@ log_debug() {
 }
 #	}}}
 
+_vimh_path_localhistory="$HOME/.vimh"
+#	validate existance: _vimh_path_localhistory
+#	{{{
+if [[ ! -f "$_vimh_path_localhistory" ]]; then
+	echo "vimh, warning, not found, _vimh_path_localhistory=($_vimh_path_localhistory)" > /dev/stderr
+fi
+#	}}}
 
-vimh_reader() {
+_vimh_path_dir_globalhistory="$mld_out_cloud_shared/combined-logs"
+_vimh_name_globalhistory="vimh.vi.txt"
+
+Vimh() {
 	#	{{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -33,14 +43,12 @@ vimh_reader() {
 	local func_help="""$func_name, $func_about
 	-v | --debug
 	-h | --help"""
+	#	parse args "$@"
 	#	{{{
 	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
 		echo "$func_help"
 		return 2
 	fi
-	#	}}}
-	#	parse args "$@"
-	#	{{{
 	for arg in "$@"; do
 		case $arg in
 			-h|--help)
@@ -55,24 +63,103 @@ vimh_reader() {
 		esac
 	done
 	#	}}}
-
 }
 
-check_sourced=1
-#	{{{
-if [[ -n "${ZSH_VERSION:-}" ]]; then 
-	if [[ ! -n ${(M)zsh_eval_context:#file} ]]; then
-		check_sourced=0
+_Vimh_get_uniquepaths() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
 	fi
-elif [[ -n "${BASH_VERSION:-}" ]]; then
-	(return 0 2>/dev/null) && check_sourced=1 || check_sourced=0
-else
-	echo "error, check_sourced, non-zsh/bash" > /dev/stderr
-	exit 2
-fi
-#	}}}
-if [[ "$check_sourced" -eq 0 ]]; then
-	vimh_reader "$@"
-fi
+	#	}}}
+	local flag_only_realpaths=1
+}
+
+_Vimh_filter_real_paths() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+
+_VimH_Update_GlobalHistory() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+_VimH_GetPath_GlobalHistory() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+_Vimh_prompt_select() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+_Vimh_truncate_paths_for_output() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+#check_sourced=1
+##	{{{
+#if [[ -n "${ZSH_VERSION:-}" ]]; then 
+#	if [[ ! -n ${(M)zsh_eval_context:#file} ]]; then
+#		check_sourced=0
+#	fi
+#elif [[ -n "${BASH_VERSION:-}" ]]; then
+#	(return 0 2>/dev/null) && check_sourced=1 || check_sourced=0
+#else
+#	echo "error, check_sourced, non-zsh/bash" > /dev/stderr
+#	exit 2
+#fi
+##	}}}
+#if [[ "$check_sourced" -eq 0 ]]; then
+#	vimh "$@"
+#fi
 
 

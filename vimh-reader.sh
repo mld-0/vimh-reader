c196a4669b53c8494cce8530c3cc5a54b90d0315
Matthew L Davis <mld.0@protonmail.com>
2022-06-06 03:31:51 +1000

    version 0.1 initial implementation
    (Appears to) work, re: read vimh, filter it, prompt user for selection,
    and navigate-to-and-open file. Limit to last 25000 entries in vimh to
    improve speed. Untested beyond rudimentary usage. Many ongoings: bash
    functions and keeping global scope clean, sh-safety settings local to
    function/script, tab-ing through options, parts of script in subshell,
    fast 'readlink -f', and unimplemented (problematic) functions.
    Vastly neater than vimh_old, still leaves something to be desired (but
    doesn't all bash?). Does vimh_old also do something akin to limiting
    input to last n lines (since it is faster than new version for whole
    file)? Continue: tests, global history option, fast realpath, cleanup.
    (Or, update vim logging plugin and existing log to use realpaths?)

--- vimh-reader.sh
+++ vimh-reader.sh
@@ -4,29 +4,64 @@
 #	{{{2
 nl=$'\n'
 tab=$'\t'
+#	{{{
 #set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
+#set -o nounset   # abort on unbound variable
+#set -o pipefail  # don't hide errors within pipes
+#	}}}
+#	Ongoings:
+#	{{{
+#	Ongoing: 2022-06-05T21:16:13AEST (how to) hide bash functions (that aren't 'Vimh') from global scope (wider shell env)
+#	Ongoing: 2022-06-05T21:16:55AEST (how to) turn sh-safety on then off (or as it was) again
+#	Ongoing: 2022-06-05T21:31:46AEST (what would be ideal is) 'local' sh-safety settings
+#	Ongoing: 2022-06-06T01:07:26AEST (how hard would it be to) tab through options (instead of typing a number?)
+#	Ongoing: 2022-06-06T01:20:42AEST could we do all/part of this in (script to be executed) (only need subshell to cd to result - implies need for handler function which recieves/does transfer to dir, last thing before (then) opening said file) <- ('_Vimh_get_uniquepaths' doesn't need to be sourced?) (could we do the same thing faster in C++?)
+#	Ongoing: 2022-06-06T01:23:39AEST ';' after command with redirection to /dev/[stderr|null]?
+#	Ongoing: 2022-06-06T02:52:22AEST for 'cd' to work, none of the function calls leading to it can be in subshells (and we would have to use temp files (or global vars, or other dubious methods) if we wanted to return data from them) [...] (is that we find we were not doing so even before considering this possible problem indiciative of <good> design (vis-a-vis dataflow)?) 
+#	Ongoing: 2022-06-06T03:07:33AEST (don't put any '|' in $HOME)
+#	}}}
 
-flag_debug=1
-log_debug() {
-	#	{{{
-	if [[ $flag_debug -ne 0 ]]; then
+flag_debug_vimh=1
+log_debug_vimh() {
+#	{{{
+	if [[ $flag_debug_vimh -ne 0 ]]; then
 		echo "$@" > /dev/stderr
 	fi
 }
 #	}}}
 
+_vimh_version="0.1"
+
+#	local history file
 _vimh_path_localhistory="$HOME/.vimh"
-#	validate existance: _vimh_path_localhistory
+
+#	NOT IMPLEMENTED global history 
+_vimh_path_dir_globalhistory="$mld_out_cloud_shared/combined-logs"
+_vimh_name_globalhistory="vimh.vi.txt"
+
+#	NOT IMPLEMENTED (slow) convert every path to realpath
+_vimh_flag_only_realpaths=0		
+
+#	path 'Vimh' uses for input
+_vimh_path_input="$_vimh_path_localhistory"
+
+#	application to open files
+_vimh_editor="$EDITOR"
+
+_vimh_lines_limit=25000
+
+#	validate existance: _vimh_path_localhistory, _vimh_path_dir_globalhistory, _vimh_editor
 #	{{{
 if [[ ! -f "$_vimh_path_localhistory" ]]; then
 	echo "vimh, warning, not found, _vimh_path_localhistory=($_vimh_path_localhistory)" > /dev/stderr
 fi
+if [[ ! -d "$_vimh_path_dir_globalhistory" ]]; then
+	echo "vimh, warning, not found, _vimh_path_dir_globalhistory=($_vimh_path_dir_globalhistory)" > /dev/stderr
+fi
+if [[ ! -x "$_vimh_editor" ]]; then
+	echo "vimh, warning, not found, _vimh_editor=($_vimh_editor)" > /dev/stderr
+fi
 #	}}}
-
-_vimh_path_dir_globalhistory="$mld_out_cloud_shared/combined-logs"
-_vimh_name_globalhistory="vimh.vi.txt"
 
 Vimh() {
 	#	{{{
@@ -40,9 +75,11 @@ Vimh() {
 	fi
 	#	}}}
 	local func_about="about"
+	local previous_flag_debug_vimh=$flag_debug_vimh
 	local func_help="""$func_name, $func_about
 	-v | --debug
-	-h | --help"""
+	-h | --help
+	--version"""
 	#	parse args "$@"
 	#	{{{
 	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
@@ -57,14 +94,29 @@ Vimh() {
 				shift
 				;;
 			-v|--debug)
-				flag_debug=1
+				flag_debug_vimh=1
+				shift
+				;;
+			--version)
+				echo "$_vimh_version"
+				return 2
 				shift
 				;;
 		esac
 	done
 	#	}}}
+
+	#	Ongoing: 2022-06-06T01:33:19AEST debug output, include time taken to run '_Vimh_get_uniquepaths'
+	local unique_files=$( _Vimh_get_uniquepaths "$_vimh_path_input" )
+	#	Ongoing: 2022-06-06T01:37:14AEST can't capture output of '_Vimh_prompt_open' as a subshell and also display output from it before prompting for input from it (that is, can't move call to '_Vimh_cd_and_open' out of it)
+	_Vimh_prompt_open "$unique_files"
+
+	flag_debug_vimh=$previous_flag_debug_vimh
 }
 
+#	Test: 2022-06-06T03:16:09AEST are _Vimh_get_uniquepaths outputs each unique?
+#	Ongoing: 2022-06-06T01:18:36AEST use of 'echo' with/without '-n'  (would it change anything) (anywhere?)
+#	Ongoing: 2022-06-05T21:28:54AEST would it be faster to filter for existance before filtering for uniqueness? [...] new vimh is somehow slower than old one?
 _Vimh_get_uniquepaths() {
 	#	{{{
 	local func_name=""
@@ -76,10 +128,257 @@ _Vimh_get_uniquepaths() {
 		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
 	fi
 	#	}}}
-	local flag_only_realpaths=1
+	local path_input="${1:-}"
+	#	validate: path_input
+	#	{{{
+	if [[ ! -f "$path_input" ]]; then
+		echo "$func_name, error, not found, path_input=($path_input)" > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	local files_list_str=$( _Vimh_read_file "$path_input"  | awk -F'\t' '{print $5}' | tac | awk '!count[$0]++' | tac )
+	#	validate non-empty: files_list_str
+	#	{{{
+	if [[ -z "$files_list_str" ]]; then
+		echo "$func_name, error, files_list_str=($files_list_str)" > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	files_list_str=$( _Vimh_filter_existing_paths "$files_list_str" )
+	#	{{{
+	if [[ ! $_vimh_flag_only_realpaths -eq 0 ]]; then
+		echo "$func_name, error, _Vimh_filter_realpaths not available" > /dev/stderr
+		return 2
+		#files_list_str=$( _Vimh_filter_realpaths "$files_list_str" | tac | awk '!count[$0]++' | tac )
+	fi
+	#	}}}
+	log_debug_vimh "$func_name, lines(files_list_str)=(`echo -n "$files_list_str" | wc -l`)"
+	echo -n "$files_list_str"
 }
 
-_Vimh_filter_real_paths() {
+_Vimh_read_file() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local path_input="${1:-}"
+	#	validate: path_input
+	#	{{{
+	if [[ ! -f "$path_input" ]]; then
+		echo "$func_name, error, not found, path_input=($path_input)" > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	cat "$path_input" | tail -n $_vimh_lines_limit
+}
+
+_Vimh_filter_existing_paths() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local paths_list_str="${1:-}"
+	local IFS_temp=$IFS; IFS=$nl;
+	local paths_list=( $( echo "$paths_list_str" ) ) 
+	IFS=$IFS_temp
+	for loop_path in "${paths_list[@]}"; do
+		if [[ -f "$loop_path" ]]; then
+			loop_path=$( echo -n "$loop_path" )
+			echo "$loop_path"
+		fi
+	done
+}
+
+#	Ongoing: 2022-06-06T02:42:37AEST can't use subshells if we want our use of 'cd' to effect the caller(?)
+_Vimh_prompt_open() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local unique_files="${1:-}"
+	local IFS_temp=$IFS; IFS=$nl;
+	local prompt_files=( $( _Vimh_truncate_paths_to_screen "$unique_files" | sed "s|$HOME|~|g" ) )
+	IFS=$IFS_temp
+	log_debug_vimh "$func_name, len(prompt_files)=(${#prompt_files[@]})"
+
+	_Vimh_print_prompt_files "${prompt_files[@]}" 
+
+	local range_max="${#prompt_files[@]}"
+	echo "Select [1, $range_max]:"
+	local user_selection_path=$( _Vimh_promptUser_selectPath "$range_max" "$unique_files" )
+	log_debug_vimh "$func_name, user_selection_path=($user_selection_path)"
+
+	_Vimh_cd_and_open "$user_selection_path"
+}
+
+#	Ongoing: 2022-06-05T20:15:18AEST how to handle 'terminal window shorter than 4 / narrower than 12' case?
+_Vimh_truncate_paths_to_screen() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local unique_files="${1:-}"
+	local output_height=$(( `tput lines` - 10 ))
+	local output_width=$(( `tput cols` - 8 ))
+	#	ensure output_height and output_width > 1
+	#	{{{
+	if [[ $output_height -lt 1 ]]; then
+		output_height=1
+	fi
+	if [[ $output_width -lt 1 ]]; then
+		output_width=1
+	fi
+	#	}}}
+	log_debug_vimh "$func_name, output_height=($output_height), output_width=($output_width)"
+
+	local IFS_temp=$IFS; IFS=$nl;
+	local prompt_files=( $( echo -n "$unique_files" | tail -n $output_height ) )
+	IFS=$IFS_temp
+	log_debug_vimh "$func_name, len(prompt_files)=(${#prompt_files[@]})"
+
+	for loop_file in "${prompt_files[@]}"; do
+		if [[ `echo -n "$loop_file" | wc -c` -ge $output_width ]]; then
+			local prepend_str="..."
+			local cut_width=$(( $output_width - `echo -n "$prepend_str" | wc -c` ))
+			loop_file="$prepend_str${loop_file: -$cut_width}"
+		fi
+		echo -n "$loop_file\n"
+	done
+}
+
+#	Test: 2022-06-06T03:23:19AEST _Vimh_print_prompt_files (and our usage of 'func "${passed[@]}"' and 'recieved=("$@")') is not tricked by filenames/filepaths with spaces
+_Vimh_print_prompt_files() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local prompt_files=( "$@" )
+	log_debug_vimh "$func_name, len(prompt_files)=(${#prompt_files[@]})"
+
+	i=${#prompt_files[@]}
+	for loop_file in "${prompt_files[@]}"; do
+		echo -n  "$i)  "
+		echo -n "$loop_file\n"
+		i=$(( $i - 1 ))
+	done
+}
+
+_Vimh_promptUser_selectPath() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local range_max="$1"
+	local unique_files="$2"
+	#	validate: range_max
+	#	{{{
+	if [[ -z `echo "$range_max" | grep "^[[:digit:]]*$"` ]]; then
+		echo "$func_name, error, invalid range_max=($range_max)"  > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	local user_selection_num=""
+	read user_selection_num
+	#	validate: user_selection_num
+	#	{{{
+	if [[ -z `echo "$user_selection_num" | grep "^[[:digit:]]*$"` ]]; then
+		echo "$func_name, error, invalid user_selection_num=($user_selection_num)" > /dev/stderr
+		return 2
+	fi
+	if [[ ! $user_selection_num -gt 0 ]] || [[ ! $user_selection_num -le $range_max ]]; then
+		echo "$func_name, error, invalid user_selection_num=($user_selection_num)" > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	log_debug_vimh "$func_name, lines(unique_files)=(`echo "$unique_files" | wc -l`)"
+	log_debug_vimh "$func_name, user_selection_num=($user_selection_num)"
+
+	local user_selection_path=""
+	user_selection_path=$( echo "$unique_files" | tail -n "$user_selection_num" | head -n 1 )
+	log_debug_vimh "$func_name, user_selection_path=($user_selection_path)"
+
+	echo "$user_selection_path"
+}
+
+_Vimh_cd_and_open() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+	local path_open="$1"
+	#	{{{
+	if [[ ! -f "$path_open" ]]; then
+		echo "$func_name, error, not found, path_open=($path_open)" > /dev/stderr
+		return 2
+	fi
+	#	}}}
+	local path_open_dir=$( dirname "$path_open" )
+
+	echo "cd $path_open_dir"
+	cd "$path_open_dir"
+
+	echo "$_vimh_editor \"$path_open\""
+	$_vimh_editor "$path_open"
+}
+
+
+
+_Vimh_Update_GlobalHistory() {
+	#	{{{
+	local func_name=""
+	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+		func_name=${funcstack[1]:-}
+	elif [[ -n "${BASH_VERSION:-}" ]]; then
+		func_name="${FUNCNAME[0]:-}"
+	else
+		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+	fi
+	#	}}}
+}
+
+_Vimh_GetPath_GlobalHistory() {
 	#	{{{
 	local func_name=""
 	if [[ -n "${ZSH_VERSION:-}" ]]; then 
@@ -93,57 +392,92 @@ _Vimh_filter_real_paths() {
 }
 
 
-_VimH_Update_GlobalHistory() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-}
 
-_VimH_GetPath_GlobalHistory() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-}
-
-_Vimh_prompt_select() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-}
-
-_Vimh_truncate_paths_for_output() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-}
+#	Ongoing: 2022-06-04T23:17:26AEST problematic functions:
+#_Vimh_filterLines_lastUnique() {
+#	#	{{{
+#	local func_name=""
+#	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+#		func_name=${funcstack[1]:-}
+#	elif [[ -n "${BASH_VERSION:-}" ]]; then
+#		func_name="${FUNCNAME[0]:-}"
+#	else
+#		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+#	fi
+#	#	}}}
+#	#	Requires reading from pipe (stdin?)
+#}
+#_Vimh_filter_realpaths() {
+#	#	{{{
+#	local func_name=""
+#	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+#		func_name=${funcstack[1]:-}
+#	elif [[ -n "${BASH_VERSION:-}" ]]; then
+#		func_name="${FUNCNAME[0]:-}"
+#	else
+#		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+#	fi
+#	#	}}}
+#	#	Ongoing: 2022-06-04T23:14:14AEST fast way to run 'readlink -f {}' on 100+ files?
+#	#	No fast way to do this?
+#	#	{{{
+#	#if [[ -f "$loop_path" ]]; then
+#	#	#echo $( _Vimh_get_fast_realpath "$loop_path" )
+#	#	#realpath "$loop_path"
+#	#	readlink -f "$loop_path"
+#	#	#local temp_PWD=$PWD
+#	#	#cd -P "`dirname "$loop_path"`"
+#	#	#local output_dir="$PWD"
+#	#	#local output_name="`basename "$loop_path"`"
+#	#	#echo "$output_dir/$output_name"
+#	#	#realpath --canonicalize-existing "$loop_path"
+#	#	#realpath "$loop_path"
+#	#	#readlink -f "$loop_path"
+#	#	#realpath "$loop_path"
+#	#	#if [[ ! -h "$loop_path" ]]; then
+#	#	#	echo "$loop_path" 
+#	#	#else
+#	#	#	readlink "$loop_path"
+#	#	#fi
+#	#	#cd "$temp_PWD"
+#	#fi
+#	#	}}}
+#}
+#_Vimh_check_contains_symlink() {
+#	#	{{{
+#	local func_name=""
+#	if [[ -n "${ZSH_VERSION:-}" ]]; then 
+#		func_name=${funcstack[1]:-}
+#	elif [[ -n "${BASH_VERSION:-}" ]]; then
+#		func_name="${FUNCNAME[0]:-}"
+#	else
+#		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
+#	fi
+#	#	}}}
+#	#	LINK: https://serverfault.com/questions/1069147/how-can-i-quickly-test-a-path-in-bash-to-determine-if-any-segment-of-it-is-a-sym
+#	#	If we can do this quickly as '[[ -L ]]', we can use 'echo' for files that pass and 'readlink -f' only where necessary
+##	{{{
+#	#local my_path="$1"
+#	#if [[ "$my_path" != "$(realpath --canonicalize-existing $my_path)" ]];then
+#	#  #echo The path $my_path is relative path or contains symlinks.
+#	#  return 1
+#	#else
+#	#  #echo The path $my_path is absolute.
+#	#  return 0
+#	#fi
+#	#local tmp="$1"
+#	#while [ "$tmp" != "." ]; do
+#	#	if [ -L "$tmp" ]; then
+#	#		#/bin/echo "$tmp - is symlink."
+#	#		return 1;
+#	#	else
+#	#		#/bin/echo "$tmp - is not symlink."
+#	#		return 0;
+#	#	fi
+#	#	tmp=$(dirname $tmp)
+#	#done
+##	}}}
+#}
 
 #check_sourced=1
 ##	{{{

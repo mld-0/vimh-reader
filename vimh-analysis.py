d96cd4721fdf0073c42428ca3aa8fd5ba9b63725
Matthew L Davis <mld.0@protonmail.com>
2022-07-26 23:07:04 +1000

    cleanup

--- vimh-analysis.py
+++ vimh-analysis.py
@@ -10,67 +10,82 @@ import datetime
 import dateutil
 import pandas as pd
 import pprint
+from typing import List
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 #   Ongoings:
 #   {{{
 #   Ongoing: 2022-07-24T00:37:00AEST when/where to modify df in place versus return modified copy?
 #   Ongoing: 2022-07-24T00:38:03AEST df rows are called records?
 #   Ongoing: 2022-07-24T02:43:38AEST 'apply(strptime)' is faster than 'pd.to_datetime()'?
+#   Ongoing: 2022-07-26T22:52:14AEST fast str2dt method(?)
 #   }}}
 
 path_input = os.path.join(os.getenv('HOME'), '.vimh')
 assert os.path.isfile(path_input)
 
-def countUniquePerDayByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
-    """Group by day and count unique values in a given column"""
+class VimhAnalysis:
+
+    #   Ongoing: 2022-07-26T22:52:45AEST 'groupByDay()' method -> possible within a df, or return list of df-s?)
+    @staticmethod
+    def groupByDay(df: pd.DataFrame):
+        """"""
+        ...
 
 
-def groupByDay(df: pd.DataFrame):
-    ...
+    @staticmethod
+    def countUniquePerDayByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
+        """Group by day and count unique values in a given column"""
+        raise NotImplementedError()
+    @staticmethod
+    def filterLastUniqueByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
+        """Keep only records which have the last instance of each given unique value in a given column"""
+        raise NotImplementedError()
+    @staticmethod
+    def substituteHomeStr(df: pd.DataFrame, cols: List[int]) -> pd.DataFrame:
+        """Replaces instances of '$HOME' with '~' in df (for given columns 'cols') (which must be strings?)"""
+        raise NotImplementedError()
+    @staticmethod
+    def filterEmptyByCol(df: pd.DataFrame, col: int):
+        """Remove records where value in given column is empty (NaN) (in-place?)"""
+        raise NotImplementedError()
 
 
-def filterLastUniqueByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
-    """Keep only records which have the last instance of each given unique value in a given column"""
+    @staticmethod
+    def parseDateTimes(df: pd.DataFrame, col: str):
+        """Convert column containing iso-datetime strings to 'date' and 'time' columns"""
+        def convertStringToDateTime(df: pd.DataFrame):
+            iso_fmt = "%Y-%m-%dT%H:%M:%S%z"
+            str2dt = lambda x: datetime.datetime.strptime(x, iso_fmt)
+            df[col] = pd.to_datetime(df[col].apply(str2dt), utc=True)
+        def splitDateTimeColumn(df: pd.DataFrame):
+            df.insert(0, 'date', df['datetime'].dt.date)
+            df.insert(1, 'time', df['datetime'].dt.time)
+            df.drop(col, axis=1, inplace=True)
+        convertStringToDateTime(df)
+        splitDateTimeColumn(df)
 
-def substituteHomeStr(df: pd.DataFrame) -> pd.DataFrame:
-    """Replaces instances of '$HOME' with '~' in df"""
+
+    #   Continue: 2022-07-26T22:50:06AEST pd.read_csv -> limit to last N lines?
+    @staticmethod
+    def read_vimh_df(path_input: str) -> pd.DataFrame:
+        columns = [ 'datetime', 'filename', 'host', 'action', 'filepath', 'realpath', ]
+        df = pd.read_csv(path_input, delimiter='\t', names=columns)
+        VimhAnalysis.parseDateTimes(df, 'datetime')
+        logging.debug("df=(%s)" % df)
+        return df
 
 
-def filterEmptyByCol(df: pd.DataFrame, col: int):
-    """Remove records where value in given column is empty (NaN)"""
-    #   (in-place?)
-
-def parseDateTimes(df: pd.DataFrame, col: str):
-    """Convert column containing strings to iso-datetimes"""
-    _fmt = "%Y-%m-%dT%H:%M:%S%z"
-    df[col] = pd.to_datetime(df[col].apply(lambda x: datetime.datetime.strptime(x, _fmt)), utc=True)
-
-def splitDateTimes(df: pd.DataFrame):
-    """Replace 'datetime' column with 'date' and 'time' columns"""
-    df.insert(0, 'date', df['datetime'].dt.date)
-    df.insert(1, 'time', df['datetime'].dt.time)
-    df.drop('datetime', axis=1, inplace=True)
-
-def read_vimh_df(path_input: str) -> pd.DataFrame:
-    columns = _getVimhColumns()
-    df = pd.read_csv(path_input, delimiter='\t', names=columns)
-
-    parseDateTimes(df, 'datetime')
-    splitDateTimes(df)
-
-    logging.debug("df=(%s)" % df)
-    return df
-
-def _getVimhColumns():
-    return [ 'datetime', 'filename', 'host', 'action', 'filepath', 'realpath', ]
 
 def runCountUniquePathsPerDay():
-    df = read_vimh_df(path_input)
+    df = VimhAnalysis.read_vimh_df(path_input)
+    ...
 
 def runFilterLastUniquePaths():
+    df = VimhAnalysis.read_vimh_df(path_input)
     raise NotImplementedError()
 
 
 if __name__ == '__main__':
     runCountUniquePathsPerDay()
+    #runFilterLastUniquePaths()
 

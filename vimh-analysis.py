b509d8d70b7e819db219499469dd0fbc403efd5d
Matthew L Davis <mld.0@protonmail.com>
2022-07-27 00:29:38 +1000

    count unique paths by day

--- vimh-analysis.py
+++ vimh-analysis.py
@@ -25,17 +25,32 @@ assert os.path.isfile(path_input)
 
 class VimhAnalysis:
 
-    #   Ongoing: 2022-07-26T22:52:45AEST 'groupByDay()' method -> possible within a df, or return list of df-s?)
+    #   Ongoing: 2022-07-26T22:52:45AEST 'groupByDay()' return nested-index df, or list of df?
     @staticmethod
-    def groupByDay(df: pd.DataFrame):
-        """"""
-        ...
-
-
-    @staticmethod
-    def countUniquePerDayByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
+    def countUniquePerDay(df: pd.DataFrame) -> pd.DataFrame:
         """Group by day and count unique values in a given column"""
-        raise NotImplementedError()
+        col_count_unique = 'filepath'
+        #   Produces result with multiindex: [date,path] = count
+        df_countUniqueByDay = df.groupby(pd.Grouper(key='date', axis=0, freq='D'))[col_count_unique].value_counts()
+        logging.debug("df_countUniqueByDay=(%s)" % df_countUniqueByDay)
+        logging.debug("df_countUniqueByDay.index=(%s)" % df_countUniqueByDay.index)
+
+        #   Iterate over unique dates
+        for date, loop_df in df_countUniqueByDay.groupby(level=0):
+            #   Unique date
+            print(date)
+            #   Remove unique date from loop_df
+            loop_df = loop_df.droplevel(0)
+            print(loop_df)
+            #   Convert 'loop_df' to List[Tuple[str,int]]
+            #loop_unique_paths = loop_df.index
+            #loop_unique_counts = loop_df.tolist()
+            #loop_unique = list(zip(loop_unique_paths, loop_unique_counts))
+            #print(loop_unique)
+
+        return df_countUniqueByDay
+
+
     @staticmethod
     def filterLastUniqueByColumn(df: pd.DataFrame, col: int) -> pd.DataFrame:
         """Keep only records which have the last instance of each given unique value in a given column"""
@@ -61,6 +76,11 @@ class VimhAnalysis:
             df.insert(0, 'date', df['datetime'].dt.date)
             df.insert(1, 'time', df['datetime'].dt.time)
             df.drop(col, axis=1, inplace=True)
+            df['date'] = pd.to_datetime(df['date'])
+            df['time'] = df['time']
+            #logging.debug("df['date']=(%s)" % df['date'])
+            #logging.debug("df['time']=(%s)" % df['time'])
+            #logging.debug("type(df['time'].at(0))=(%s)" % type(df['time'][0]))
         convertStringToDateTime(df)
         splitDateTimeColumn(df)
 
@@ -70,6 +90,7 @@ class VimhAnalysis:
     def read_vimh_df(path_input: str) -> pd.DataFrame:
         columns = [ 'datetime', 'filename', 'host', 'action', 'filepath', 'realpath', ]
         df = pd.read_csv(path_input, delimiter='\t', names=columns)
+        #df = df.tail(10000)
         VimhAnalysis.parseDateTimes(df, 'datetime')
         logging.debug("df=(%s)" % df)
         return df
@@ -78,7 +99,7 @@ class VimhAnalysis:
 
 def runCountUniquePathsPerDay():
     df = VimhAnalysis.read_vimh_df(path_input)
-    ...
+    df_countUniqueByDay = VimhAnalysis.countUniquePerDay(df)
 
 def runFilterLastUniquePaths():
     df = VimhAnalysis.read_vimh_df(path_input)
